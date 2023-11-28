9cce6cf36d2f87b66afa6b67a46d254ae7bf8496
Berthelotflorian1 <68320944+Berthelotflorian1@users.noreply.github.com>
2023-11-28 16:40:08 +0100

    Test Composite

--- myApp/Composite/Component.cs
+++ myApp/Composite/Component.cs
@@ -17,3 +17,92 @@ namespace Composite
         public abstract void Display(int depth);
     }
 }
+
+
+
+
+
+
+
+
+
+
+using System;
+using System.Collections.Generic;
+
+// Interface pour les composants (Entreprise et Filiale)
+public interface IComponent
+{
+    void DisplayInfo();
+}
+
+// Classe représentant une entreprise
+public class Entreprise : IComponent
+{
+    private string nom;
+    private List<IComponent> filiales = new List<IComponent>();
+
+    public Entreprise(string nom)
+    {
+        this.nom = nom;
+    }
+
+    public void AjouterFiliale(IComponent filiale)
+    {
+        filiales.Add(filiale);
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Entreprise: {nom}");
+        Console.WriteLine("Filiales:");
+        foreach (var filiale in filiales)
+        {
+            filiale.DisplayInfo();
+        }
+        Console.WriteLine();
+    }
+}
+
+// Classe représentant une filiale
+public class Filiale : IComponent
+{
+    private string nom;
+
+    public Filiale(string nom)
+    {
+        this.nom = nom;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"- Filiale: {nom}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Création d'une entreprise principale
+        Entreprise entreprisePrincipale = new Entreprise("Entreprise Principale");
+
+        // Création de filiales
+        Filiale filiale1 = new Filiale("Filiaire 1");
+        Filiale filiale2 = new Filiale("Filiaire 2");
+
+        // Ajout des filiales à l'entreprise principale
+        entreprisePrincipale.AjouterFiliale(filiale1);
+        entreprisePrincipale.AjouterFiliale(filiale2);
+
+        // Création de filiales pour la filiale 1
+        Filiale filiale1_1 = new Filiale("Filiaire 1.1");
+        Filiale filiale1_2 = new Filiale("Filiaire 1.2");
+
+        // Ajout des filiales à la filiale 1
+        filiale1.AjouterFiliale(filiale1_1);
+        filiale1.AjouterFiliale(filiale1_2);
+
+        Console.ReadLine();
+    }
+}
